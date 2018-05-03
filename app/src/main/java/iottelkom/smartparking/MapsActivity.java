85b375e9191ec431665df07ff3cc69abf98a6255
lusanmaulana <lusan.maulana@yahoo.co.id>
2018-05-03 10:14:25 +0700

    Added location function

--- app/src/main/java/iottelkom/smartparking/MapsActivity.java
+++ app/src/main/java/iottelkom/smartparking/MapsActivity.java
@@ -1,18 +1,111 @@
 package iottelkom.smartparking;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.LocationListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+public class MapsActivity extends FragmentActivity implements
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener{
+
+    private static final int MY_PERMISSIONS_REQUEST = 99;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    LocationRequest mLocationRequest;
     private GoogleMap mMap;
+    private Marker mNow;
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @SuppressLint("RestrictedApi")
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        //10 detik sekali minta lokasi (10000ms = 10 detik)
+        mLocationRequest.setInterval(10000);
+        //tapi tidak boleh lebih cepat dari 5 detik
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+
+    public void ambilLokasi() {
+        /* mulai Android 6 (API 23), pemberian persmission
+           dilakukan secara dinamik (tdk diawal)
+           untuk jenis2 persmisson tertentu, termasuk lokasi
+        */
+        // cek apakah sudah diijinkan oleh user, jika belum tampilkan dialog
+        // pastikan permission yg diminta cocok dgn manifest
+        if (ActivityCompat.checkSelfPermission (this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED )
+        {
+            //belum ada ijin, tampilkan dialog
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_REQUEST);
+            return;
+        }
+        //ambil lokasi terakhir
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                ambilLokasi();
+            } else {
+                //permssion tidak diberikan, tampilkan pesan
+                AlertDialog ad = new AlertDialog.Builder(this).create();
+                ad.setMessage("Tidak mendapat ijin, tidak dapat mengambil lokasi");
+                ad.show();
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +115,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        buildGoogleApiClient();
+        createLocationRequest();
     }
 
 
@@ -38,9 +133,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in GIK and move the camera
+        LatLng gik = new LatLng(-6.860418, 107.589889);
+        mNow = mMap.addMarker(new MarkerOptions().position(gik).title("Marker in GIK"));
+
+        // batas UPI, kiri-bawah dan kanan-atas (urutan harus spt itu)
+        LatLngBounds UPI = new LatLngBounds(
+                new LatLng(-6.863273, 107.587212),new LatLng(-6.858025, 107.597839));
+
+        // Set kamera sesuai batas UPI
+        int width = getResources().getDisplayMetrics().widthPixels;
+        int height = getResources().getDisplayMetrics().heightPixels;
+        int padding = (int) (width * 0.12); // offset dari edges
+        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(UPI, width, height, padding));
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        ambilLokasi();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mNow.setPosition(new LatLng(location.getLatitude(),location.getLongitude()));
     }
 }
